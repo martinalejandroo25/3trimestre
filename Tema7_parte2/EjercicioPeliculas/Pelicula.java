cf3ac76df30ac9f396a79fdda244e722adf94e75
martinalejandroo25 <martinalejandroo25@gmail.com>
2024-04-04 21:07:15 +0200

    xd css y html

--- Tema7_parte2/EjercicioPeliculas/Pelicula.java
+++ Tema7_parte2/EjercicioPeliculas/Pelicula.java
@@ -1,2 +1,16 @@
-package Tema7_parte2.EjercicioPeliculas;public class Pelicula {
+package Tema7_parte2.EjercicioPeliculas;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Pelicula {
+    private String id;
+    private String titulo;
+    private Integer year;
+    private ArrayList<Genero> generos;
+    private  ArrayList<Director> directores;
+
+
+
+
 }
