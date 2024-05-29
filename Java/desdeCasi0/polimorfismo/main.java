e2219077f982bdd0f03cba8ccabc67b1481565d3
martinalejandroo25 <martinalejandroo25@gmail.com>
2024-05-29 18:00:19 +0200

    commits de cosas

--- Java/desdeCasi0/polimorfismo/main.java
+++ Java/desdeCasi0/polimorfismo/main.java
@@ -2,7 +2,10 @@ package polimorfismo;
 
 public class main {
     public static void main(String[] args) {
-        AsientoBusiness asiento1 = new AsientoBusiness(1L, 125, 5, "A", pasajero1, true);
-        Pasajero pasajero1 = new Pasajero(1L, "")
+
+        /*Long id, String nombre, String apellido, String email, String direccion, String ciudad, String pais, String telefono, String preferenciasComida, String restriccionesMedicas, Asiento asiento*/
+        Asiento asiento1 = new AsientoBusiness(1L, 125.5, 5, "A", pasajero1, true);
+
+        Pasajero pasajero1 = new Pasajero(1L,"Juan", "perez", "juanperez@gmail.com", "antonio machado 61", "almeria", "españa", "123123123", "vegetariano", "alergia frutos secos", asiento1 );
     }
 }
